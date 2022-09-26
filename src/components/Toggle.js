656db85458b0f5931ce0275d5df8a8f902294da0
steve <stevemighty2030@gmail.com>
2022-09-26 19:55:31 +0300

    the assigment is complete

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Toggle() {
-  return <button>OFF</button>;
+function Toggle({fontColor}) {
+
+  const[btnState, changeState] = useState(false);
+
+  function handleButtonState() {
+    changeState((btnState) => !btnState);
+  }
+  const color = btnState ? "red" : "blue";
+  return <button onClick={handleButtonState} style={{background:color, color:fontColor, fontWeight:"bold",borderStyle:"none", borderRadius:"10px"}}>{btnState ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
